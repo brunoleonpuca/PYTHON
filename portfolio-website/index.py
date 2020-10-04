aeab7dcd5bcdcb0ba80ac31bfb76a5d3ed2a3155
Bruno Leon Pucarelli <brunopuca1@gmail.com>
2020-10-04 13:12:38 -0300

    set js event listener and added to index.html

--- portfolio-website/index.py
+++ portfolio-website/index.py
@@ -1,19 +1,7 @@
 from flask import Flask , render_template
 
-app = Flask(__name__, static_url_path='/static') 
-
+app = Flask(__name__) 
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
     return render_template("index.html")
-
-
-@app.route('/json')
-def json():
-    return render_template('json.html')
-
-#background process happening without any refreshing
-@app.route('/background_process_test')
-def background_process_test():
-    print ("Hello")
-    return ("nothing")
